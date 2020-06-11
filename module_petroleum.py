3cf0c8f42b3ea94e024ed18450fc6d34ef315435
MauroMVI <mauromvi100695@gmail.com>
2020-06-11 18:42:04 +0300

    I kind of understood what's happening and that the zip file is saved in z, now I need to extract the csv file from it.

--- module_petroleum.py
+++ module_petroleum.py
@@ -5,16 +5,14 @@ import csv
 
 
 
-def download_zip(url, filename):
-    r = requests.get(url)
-    # z = zipfile.ZipFile(io.BytesIO(r.content))
-    # with z.open(filename, 'r') as f:
-    #     reader = zip.read(filename)
-    #     for row in reader:
-    #         print(row)
+def download_zip(url, save_path):
+    r = requests.get(url, stream=True)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extract(filename)
 
 
 
 url = 'https://sih.hidrocarburos.gob.mx/downloads/PRODUCCION_POZOS.zip'
 filename = 'POZOS_COMPILADO.csv'
+save_path = '/home/mmvi/mexico/petroleum/data'
 download_zip(url, filename)
