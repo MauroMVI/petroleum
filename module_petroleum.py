c63d3153ed524b9850c78e0f04feb331b15bb781
MauroMVI <mauromvi100695@gmail.com>
2020-06-11 17:58:12 +0300

    I messed up.

---
+++ module_petroleum.py
@@ -0,0 +1,21 @@
+import requests
+import zipfile
+import io
+import csv
+
+
+
+def download_zip(url, filename):
+    r = requests.get(url)
+    print(r.content)
+    # z = zipfile.ZipFile(io.BytesIO(r.content))
+    # with z.open(filename, 'r') as f:
+    #     reader = zip.read(filename)
+    #     for row in reader:
+    #         print(row)
+
+
+
+url = 'https://sih.hidrocarburos.gob.mx/downloads/PRODUCCION_POZOS.zip'
+filename = 'POZOS_COMPILADO.csv'
+download_zip(url, filename)
