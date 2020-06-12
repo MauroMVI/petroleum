6bf43d605c11f261f649ee9d04a9b3ec87135e82
MauroMVI <mauromvi100695@gmail.com>
2020-06-12 15:48:38 +0300

    Added comments and docstrings to functions.

--- module_petroleum.py
+++ module_petroleum.py
@@ -4,6 +4,7 @@ import io
 import csv
 import shutil
 from datetime import datetime
+import pandas as pd
 
 
 
@@ -66,6 +67,19 @@ def move_data(path_src, path_destination):
     # This function will rename the file as well.
     shutil.move(path_src, path_destination)
 
+def download_data(url, filename):
+    """
+    This function calls all the other functions needed to download the data,
+    save it at desired location and rename the file.
+    """
+    date = get_strdate()
+    path_src, path_destination = data_paths(date, filename)
+    download_zip(url, filename)
+    shutil.move(path_src, path_destination)
+
+    # Returns full path of csv file.
+    return path_destination
+
 def prep_data_for_pd(path_destination, encoding="ISO-8859-1"):
     """
     This function gets the number of rows to skip to reach the column headers of
@@ -77,7 +91,8 @@ def prep_data_for_pd(path_destination, encoding="ISO-8859-1"):
     filename = f'{path_destination}'
     # Initializes variables that will help create DataFrame.
     row_index = 0
-    skiprows = [0]
+    skiprow = [0]
+    dtypes = {}
     # Opens file using with statement to lower memory usage.
     with open(filename, encoding=encoding) as f:
         #Assigns the iterator obj. to reader.
@@ -94,26 +109,48 @@ def prep_data_for_pd(path_destination, encoding="ISO-8859-1"):
                 # While we reach the headers, this get the indexes for each line
                 # to be skipped in order to create the correct DataFrame.
                 row_index += 1
-                skiprows.append(row_index)
+                skiprow.append(row_index)
                 continue
         # Makes all headers to uppercase for aesthetics purposes.
-        header_rows = [element.upper() for element in header_lower]
+    header_rows = [element.upper() for element in header_lower]
+    # Assigns the dtype of each column,
+    # Do not know how to pre-assign the dtype date.
+    for header in header_rows:
+        if '(MBD)' in header:
+            dtypes[header] = 'float64'
+        elif'(MMPCD)' in header:
+            dtypes[header] = 'float64'
+        else:
+            dtypes[header] = 'str'
+
     # Returns the headers of DataFrame in uppercase and number of rows to skip.
-    return header_rows, skiprows
+    return header_rows, skiprow, dtypes
 
-def download_data(url, filename):
+def create_dataframe(path_destination, header_rows, skiprow, 
+                    dtypes, encoding="ISO-8859-1", dateformat='%d-%m-%Y'
+                    ):
     """
-    This function calls all the other functions needed to download the data,
-    save it at desired location and rename the file.
+    This function uses the information returned from prep_data_for_pd() to 
+    create the DataFrame object. path_destination is where the file is saved.
     """
-    date = get_strdate()
-    path_src, path_destination = data_paths(date, filename)
-    download_zip(url, filename)
-    shutil.move(path_src, path_destination)
 
-url = 'https://sih.hidrocarburos.gob.mx/downloads/PRODUCCION_POZOS.zip'
-filename = 'POZOS_COMPILADO.csv'
-download_data(url, filename)
-# header_rows, skiprows = prep_data_for_pd(path_destination)
-# print(header_rows)
-# print(skiprows)
+    # Creates DataFrame
+    df = pd.read_csv(path_destination, sep=',', header=0, 
+                    names=header_rows, dtype=dtypes, skiprows=skiprow,
+                    encoding=encoding,
+                    )
+    # Sets the date column to a date type with a desired format.
+    df['FECHA'] = pd.to_datetime(df['FECHA'], format=dateformat)
+
+    # Returns DataFrame
+    return df
+
+def csv_to_dataframe(path_destination):
+    header_rows, skiprow, dtypes = prep_data_for_pd(path_destination)
+    df = create_dataframe(path_destination, header_rows, skiprow, dtypes)
+
+    return df
+
+path_destination = 'data/20200612_POZOSCOMPILADO.csv'
+df = csv_to_dataframe(path_destination)
+print(df)
